8e9b41322b3257906d6f1d56a8e884529c06883f
Alejandro <molina.ramos@hotmail.com>
2022-07-23 19:42:57 +0100

    añado ejercicio

--- Tema 1/Program.cs
+++ Tema 1/Program.cs
@@ -1,10 +1,16 @@
 ﻿
 Console.WriteLine("Introduce tu nombre:");
 string nombre = Console.ReadLine();//Esto lee y guarda lo que el usuario escribe
-Console.WriteLine("Hola, " + nombre);
+//Console.WriteLine("Hola, " + nombre);
 
 //Edad?
+Console.WriteLine("Introduce tu edad:");
+int edad = int.Parse(Console.ReadLine());
 
 //Ciudad
+Console.WriteLine("Introduce tu ciudad:");
+string ciudad = Console.ReadLine();
 
 //Presentar mensaje "Hola, NOMBRE. Tienes AÑOS. Vives en Ciudad.
+Console.WriteLine("Hola, " + nombre + ". Tienes " + edad + " años. Vives en " + ciudad);
+
