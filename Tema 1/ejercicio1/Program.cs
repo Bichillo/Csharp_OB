d945b6f237a4c6e0a59455b3801a4baa47e2d9be
Alejandro <molina.ramos@hotmail.com>
2022-07-23 20:31:12 +0100

    AGREGO EJERCICIOS TEMA 1

---
+++ Tema 1/ejercicio1/Program.cs
@@ -0,0 +1,20 @@
+﻿//-------------------------------EJERCICIO 1----------------
+
+//Escribe un programa que reciba tu nombre y lo escriba por consola.
+
+//Console.WriteLine("Como te llamas?:");
+//string nombre = Console.ReadLine();
+//Console.WriteLine(nombre);
+
+
+//-------------------------------EJERCICIO 2-------------------------------
+
+//Escribe un programa que tome la hora y la escriba por pantalla.
+
+string hora = DateTime.Now.ToString("HH:mm:ss");
+Console.WriteLine(hora);
+
+
+//-----------------------------------------PRACTICA ADICIONAL-----------------------------------
+DateTime holita = DateTime.Now;
+Console.WriteLine(holita.ToString("f"));
