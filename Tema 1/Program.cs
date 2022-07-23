d00e247683dac8a18c42c7eb7e5fd33a818f5e82
Alejandro <molina.ramos@hotmail.com>
2022-07-23 19:34:35 +0100

    añado tema 1

---
+++ Tema 1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Introduce tu nombre:");
+string nombre = Console.ReadLine();//Esto lee y guarda lo que el usuario escribe
+Console.WriteLine("Hola, " + nombre);
+
+//Edad?
+
+//Ciudad
+
+//Presentar mensaje "Hola, NOMBRE. Tienes AÑOS. Vives en Ciudad.
