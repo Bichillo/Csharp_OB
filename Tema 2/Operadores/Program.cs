9cfca7d54e2e765f5d28a1e515e4e5d56c59884e
Alejandro <molina.ramos@hotmail.com>
2022-07-25 20:41:20 +0100

    agrego mas contenido al tema 2

---
+++ Tema 2/Operadores/Program.cs
@@ -0,0 +1,13 @@
+﻿//Operadores
+//Aritmeticos: + - * % ++ --
+
+int i = 0;
+Console.WriteLine("Suma: " + i + 1);
+Console.WriteLine("Autoincrmento: " + i++);
+
+//-----------------------------------Ejercicio por cuenta propia--------------------------
+for (int j = 0; j < 10; j++)
+{
+    Console.WriteLine(j+1);//Para que el contador empiece desde 1. Si no pongo esto empieza a contar desde 0.
+}
+//-------------------------------fin-----------------------------------------------
